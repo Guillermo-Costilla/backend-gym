3e24e712a42335c03d4aea61f4099e1341eb014f
Guillermo Costilla <gcostilla96@gmail.com>
2025-10-19 22:02:17 -0300

    mailer cfg

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -5,14 +5,14 @@ dotenv.config();
 export const transporter = nodemailer.createTransport({
   service: "gmail", // o SMTP personalizado
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: process.env.EMAIL_AUTH_USER,
+    pass: process.env.EMAIL_AUTH_PASS,
   },
 });
 
 export async function enviarEmail({ to, subject, html }) {
   return transporter.sendMail({
-    from: `"CRM Gym" <${process.env.MAIL_USER}>`,
+    from: `"CRM Gym" <${process.env.EMAIL_AUTH_USER}>`,
     to,
     subject,
     html,
